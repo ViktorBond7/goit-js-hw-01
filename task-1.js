5b1aaa6badce3e7a57dcc44e802419b10837800b
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2024-11-27 12:44:40 +0100

    task3

--- task-1.js
+++ task-1.js
@@ -2,11 +2,11 @@
 
 let arr1 = [1, 1, 1, 0, 0, 1, 1];
 
-const foo = (arr) => {
+const foo = arr => {
   let maxLendx = 0;
   let thisLendx = 0;
 
-  arr.forEach((element) => {
+  arr.forEach(element => {
     if (element === 1) {
       thisLendx++;
     } else {
@@ -18,29 +18,45 @@ const foo = (arr) => {
 };
 // console.log(foo(arr1));
 
-const string = "([{}])";
+// const string = "([{}])";
 
-const test = (str) => {
-  let index = [];
+// const test = (str) => {
+//   let index = [];
 
-  for (let i = 0; i < str.length; i++) {
-    const element = str[i];
+//   for (let i = 0; i < str.length; i++) {
+//     const element = str[i];
 
-    if (element === "[" || element === "(" || element === "{") {
-      index.push(element);
-    } else {
-      if (
-        index.length === 0 ||
-        (element === "]" && index[index.length - 1] !== "[") ||
-        (element === ")" && index[index.length - 1] !== "(") ||
-        (element === "}" && index[index.length - 1] !== "{")
-      ) {
-        return false;
-      }
-      index.pop();
-    }
-  }
-  return !index.length;
-};
+//     if (element === "[" || element === "(" || element === "{") {
+//       index.push(element);
+//     } else {
+//       if (
+//         index.length === 0 ||
+//         (element === "]" && index[index.length - 1] !== "[") ||
+//         (element === ")" && index[index.length - 1] !== "(") ||
+//         (element === "}" && index[index.length - 1] !== "{")
+//       ) {
+//         return false;
+//       }
+//       index.pop();
+//     }
+//   }
+//   return !index.length;
+// };
 
-console.log(test(string));
+// console.log(test(string));
+
+// for (var i = 0; i < 10; i++) {
+//   setTimeout(j => {
+//     console.log(j);
+//   }, 1000);
+// }
+i;
+for (var i = 1; i <= 3; i++) {
+  setTimeout(
+    j => {
+      console.log(j); // Через замикання i буде дорівнювати 4 для всіх викликів
+    },
+    1000,
+    i
+  );
+}
