64751f4c0b9d740853ee186ebaf3be044371f32a
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2024-11-30 11:00:23 +0100

    html

--- task-1.js
+++ task-1.js
@@ -49,15 +49,40 @@
 //   return true;
 // };
 
+const cheking = arr => {
+  const countMap = {};
+
+  arr.forEach(element => {
+    countMap[element] = (countMap[element] || 0) + 1;
+  });
+  return countMap;
+};
+
 function isSimilar(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
+  if (arr1.length !== arr2.length) return false;
+
+  const const1 = cheking(arr1);
+  const const2 = cheking(arr2);
+
+  for (const i in const1) {
+    if (const1[i] !== const2[i]) return false;
   }
-  for (current of arr1) {
-    if (!arr2.includes(current)) {
-      return false;
-    }
-  }
+
   return true;
 }
-console.log(isSimilar([2, 2, 3], [3, 2, 1]));
+
+console.log(isSimilar([1, 2, 3], [3, 2, 1]));
+
+const parent = {
+  name: 'Stacey',
+  surname: 'Moore',
+  age: 54,
+  heritage: 'Irish',
+};
+
+const child = Object.create(parent);
+
+child.name = 'Jason';
+child.age = 27;
+
+console.log('child', child.heritage);
